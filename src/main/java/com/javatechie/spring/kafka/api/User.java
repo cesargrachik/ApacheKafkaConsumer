6aebe13725d61adc52568adda238f8229229a0e5
Cesar Grachik <50838423+cesargrachik@users.noreply.github.com>
2019-08-02 14:40:35 -0400

    Api Kafka Consumer
    Api Kafka Consumer

---
+++ src/main/java/com/javatechie/spring/kafka/api/User.java
@@ -0,0 +1,35 @@
+package com.javatechie.spring.kafka.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	private int id;
+	private String name;
+	private String[] address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getAddress() {
+		return address;
+	}
+	public void setAddress(String[] address) {
+		this.address = address;
+	}
+
+}
